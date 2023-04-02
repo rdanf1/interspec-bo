82f3f75df47dcb4236b69dc68eb336a670f868e6
rdanf1 <reylet.daniel@gmail.com>
2023-04-02 23:19:26 +0200

    Beginning .../en/4.1/intro/tutorial04/

--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,30 @@
+from django.http import Http404
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from .models import Question
+
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+# Or use shortcut in 3 lines (try is hidden)...
+# (replace import clause with : "from django.shortcuts import get_object_or_404, render")
+#def detail(request, question_id):
+#    question = get_object_or_404(Question, pk=question_id)
+#    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
